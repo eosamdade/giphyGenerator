584118f79d724e80a67511a7e8caa556aa47b0b7
Ebenezer Osam Adade <eosamdade@gmail.com>
2019-07-21 09:44:34 -0500

    working on the script, got response from the gihy api

--- js/logic.js
+++ js/logic.js
@@ -1,3 +1,10 @@
-var APIKey = '';
-var queryURL = '' + APIKey;
+var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=FAFrT5yn0MYLm6Ct7Es9VJtaAvgkfRnS&q=javascript&limit=25&offset=0&rating=G&lang=en'
+
+$.ajax({
+    url:queryURL,
+    method:'GET',
+}).then(function(response){
+    console.log(response);
+})
+
 
