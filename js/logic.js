226aa60fdd746e9b0ac4cce2e3bd79d252464703
Ebenezer Osam Adade <eosamdade@gmail.com>
2019-07-23 23:05:56 -0500

    worked on finalizing js and html

--- js/logic.js
+++ js/logic.js
@@ -1,59 +1,88 @@
 
 var science = ["space","Albert Einstein", "biology","physics","fireworks","Niel Degrass Tyson","astrophysics","planets","black hole","cyclone","placebo","absolute zero","the big bang theory","chemistry","solar","Matter","gravity","astronomy","Isaac Newton","Portal 2","rick and morty","apollo 11","aliens"];
 
-$("#giphySearch").on("keyup",function(event){
-    
-    
-    var newword = $(this).val();
-    console.log(newword)
-    
+$("#add-button").on("submit",function(){
+    event.preventDefault();
+    newSearch();
+    giphyButtonGenerator();
+    $("#add-button")[0].reset();
 })
+
+function newSearch () {
+    var search = $("#giphySearch").val();
+    console.log(search);
+
+    science.push(search);
+    console.log(science);
+} 
     
-for (var i = 0; i < science.length; i++) {
-    console.log(science[i]);
-    var button = $("#giphyButton");
-    button.append(`<button class="button" data-science="${science[i]}">${science[i]}</button>`);
+function giphyButtonGenerator (){
+    $("#giphyButton").empty();    
 
-    $("button").on("click",function(){
-        console.log("listening")
-        
-        var buttonText = '';
-        buttonText = $(this).attr("data-science");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=FAFrT5yn0MYLm6Ct7Es9VJtaAvgkfRnS&q="+ buttonText +"&limit=25&offset=0&rating=G&lang=en";
-        
-           
-        $.ajax({
-            url:queryURL,
-            method:'GET',
-        }).then(function(response){
-            console.log(response);
-            
-            var results = response.data;
-            
-            for(var i = 0; i < results.length; i++){
-                var scienceDiv = $('div');
-                var p = $('<p class: rating>').text('Rating '+results[i].rating);
-                var scienceImage = $('<img>');
-                scienceImage.attr('src',results[i].images.fixed_height.url);
-            
-                scienceDiv.append(p);
-                scienceDiv.append(scienceImage);
-            
-                $("#giphShow").append(scienceDiv);
-            }
-            
-        });    
-      
-    });
+    for (var i = 0; i < science.length; i++) {
 
+        $("#giphyButton").append(`<button class="data-button" data-science="${science[i]}">${science[i]}</button>`);
+    }
 }
 
+giphyButtonGenerator()
+
+//delegated event
+$(document).on("click",".data-button",function(){
+    console.log("listening")
+    
+    var buttonText = '';
+    buttonText = $(this).attr("data-science");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=FAFrT5yn0MYLm6Ct7Es9VJtaAvgkfRnS&q="+ buttonText +"&limit=25&offset=0&rating=G&lang=en";
+    
+       
+    $.ajax({
+        url:queryURL,
+        method:'GET',
+    }).then(function(response){
+        console.log(response);
+        
+        var results = response.data;
+        
+        //clear Div before creating buttons
+        $("#giphShow").empty()
+
+        //create buttons
+
+        for(var i = 0; i < results.length; i++){
+            var scienceDiv = $('div');
+            var p = $('<p class: rating>').text('Rating '+results[i].rating);
+            var scienceImage = $('<img class="size">');
+            scienceImage.attr('src',results[i].images.fixed_height.url);
+            scienceImage.attr('still',results[i].images.fixed_height.url);
+            scienceImage.attr('animated',results[i].images.fixed_height.url);
+
+            //creat a class on the image so it is clickable
+        
+        //store the animated img in an attr called animated
+        //store the still image in an attr called still
+        //use those 2 attr to switch in and out of the source attr
+        //create a state attr and set it to still orginally
+
+            scienceDiv.append(p);
+            scienceDiv.append(scienceImage);
+        
+            $("#giphShow").append(scienceDiv);
+        }
+        
+    });    
+  
+});
 
 
 
 
 
+//new click event for each giph
 
 
+//get the url for still 
+//get the url for aminimated
+//on click switch url back and forth - state
 
 
